b109fe5e692215beca31ed24566a5dbe2959e37c
pipopa123 <inouemadoka.yuan@gmail.com>
2023-09-06 22:48:49 +0900

    第3回講義提出課題　 discord account: pipo
    Main.java　にコレクションのネスト
    Exception.java に例外を起こしてみたソースコードを入力しています
    
    不具合があるかもしれませんが
    ご確認いただきますようよろしくお願いします。

---
+++ src/main/java/com/example/list/Exception.java
@@ -0,0 +1,9 @@
+package com.example.list;
+
+import java.io.FileWriter;
+
+public class Exception {
+    public static void main(String[]args){
+        FileWriter fw = new FileWriter("ExceptionExample.text");
+    }
+}
